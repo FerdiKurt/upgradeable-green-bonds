004b0e6065d3645d01066e0e08c56033a5fbf1a2
Ferdi Kurt <ferdi.kurt111@gmail.com>
2025-05-13 19:53:32 +0300

    A comprehensive smart contract implementing a green bond to support climate and environmental projects

---
+++ src/GreenBonds.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+/// @title Enhanced GreenBonds
+/// @notice A comprehensive smart contract implementing a green bond to support climate and environmental projects
+/// @dev Uses AccessControl for role-based permissions
