764740d8126385b5e0430ffdd54c4f8ec96f464e
Ferdi Kurt <ferdi.kurt111@gmail.com>
2025-05-28 19:34:25 +0300

    Unit test imports, payment token created

---
+++ test/GreenBonds.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "../src/GreenBonds.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+// Mock ERC20 token for testing
+contract MockERC20 is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 100000000 * 10**18); // Mint 100M tokens
+    }
+    
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
+
